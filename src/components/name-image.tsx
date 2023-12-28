79df82f47f00ff62c5b5819945e2dc4e4de1778d
Ram Goel <rgoel766@gmail.com>
2023-12-28 15:42:10 +0530

    style: home text update

--- src/components/name-image.tsx
+++ src/components/name-image.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 let sizeMap: any = {
-    "sm": '25px',
-    "md": '40px',
-    "lg": '60px',
+    "sm": 'h-[25px] w-[25px]',
+    "md": 'h-[40px] w-[40px]',
+    "lg": 'h-[60px] w-[60px]',
 }
 const NameImage = ({ text, size='sm' }: { text: string, size?:string }) => {
    
     return (
-        <div className={`hidden sm:flex w-[${sizeMap[size]}] h-[${sizeMap[size]}] justify-center flex items-center rounded-full bg-blue-600`}>
+        <div className={`hidden sm:flex ${sizeMap[size]} justify-center flex items-center rounded-full bg-blue-600`}>
             <h1 className='font-bold text-white'>
                 {text[0].toUpperCase()}
             </h1>
